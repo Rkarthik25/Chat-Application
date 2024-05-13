7bc803b5382dc219c6652eef7bc89390526b41e6
Rkarthik25 <karthijan111@gmail.com>
2024-05-13 19:54:07 +0530

    Deployment

--- Front-end/src/context/SocketContext.jsx
+++ Front-end/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5008", {
+      const socket = io("https://chat-application-kgl2.onrender.com/login", {
         query: {
           userId: authUser._id,
         },
